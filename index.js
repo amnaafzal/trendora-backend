41340ccb27d54bd8577fea2a3a97038ca269f1ab
Amna Afzal <amnaafzal605@gmail.com>
2025-09-22 16:44:40 +0500

    hashed the password

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config()
 const port = process.env.PORT || 5000;
 
 
-app.use(express.json({limit: "24mb"}))
+app.use(express.json({ limit: "24mb" }))
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
@@ -17,8 +17,6 @@ app.use(cors({
   credentials: true
 }))
 app.use(cookieParser())
-// app.use(bodyParser.urlencoded())
-// app.use(bodyParser.json())
 
 
 // all routes here  
@@ -31,9 +29,9 @@ async function main() {
 }
 
 main().then(() => {
-  console.log("✅ Connected to MongoDB");
+  console.log("Connected to MongoDB");
 }).catch(err => {
-  console.error("❌ MongoDB connection error:", err);
+  console.error("MongoDB connection error:", err);
 });
 
 
--- src/users/user.model.js
+++ src/users/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   username: {type: String, unique: true, required: true},
@@ -10,6 +11,24 @@ const userSchema = new mongoose.Schema({
   bio: {type: String, maxLength:200},
   date: {type: Date, default: Date.now()}
 });
+
+userSchema.pre('save', async function(next){
+
+    if(!this.isModified('password'))
+        return next()
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+        next()
+    } catch (error) {
+        next(error)
+        
+    }
+
+
+})
+
 
 
 const user = mongoose.model('user', userSchema);
