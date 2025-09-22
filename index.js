17d89f82447a02eafa972604bc7fa5ea39eb11da
Amna Afzal <amnaafzal605@gmail.com>
2025-09-22 12:00:20 +0500

    installed dotev, body parser and cookie parser

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const mongoose = require('mongoose')
 const app = express()
 var cors = require('cors')
 const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 dotenv.config()
 const port = process.env.PORT || 5000;
 
 
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 
 async function main() {
   await mongoose.connect(process.env.MONGODB_URI)
